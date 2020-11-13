5328204e8d8d144c2a9d40e59485d293df2c1405
Santiago Solana <santiago.sjs@gmail.com>
2020-11-13 17:35:18 -0500

    db scheema set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Burgie', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lil Burgie', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monster Burgie', FALSE);
